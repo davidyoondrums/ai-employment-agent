e94778cded701c381169574d8029bee3e11fef07
dyoon <11621570+davidyoondrums@users.noreply.github.com>
2023-04-16 01:29:46 -0400

    fix: change mode back to text-davinci-003

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -136,7 +136,7 @@ export default async function handler(req: NextRequest) {
     `
 
     const completionOptions: CreateCompletionRequest = {
-      model: 'gpt-3.5-turbo',
+      model: 'text-davinci-003',
       prompt,
       max_tokens: 512,
       temperature: 0,
