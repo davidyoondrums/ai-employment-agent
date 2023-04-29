ff0cd38c736cf9386043d5b61477e9e97377c8a4
dyoon <11621570+davidyoondrums@users.noreply.github.com>
2023-04-28 22:18:22 -0400

    fix: synatax error

--- pages/api/vector-search.ts
+++ pages/api/vector-search.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from 'next/server'
 import { createClient } from '@supabase/supabase-js'
 import { codeBlock, oneLine } from 'common-tags'
 import GPT3Tokenizer from 'gpt3-tokenizer'
-import { CreateCompletionRequest, ChatCompletionResponseMessage } from 'openai'
+import { CreateCompletionRequest, CreateChatCompletionRequest } from 'openai'
 import { ApplicationError, UserError } from '@/lib/errors'
 
 // OpenAIApi does currently not work in Vercel Edge Functions as it uses Axios under the hood.
@@ -135,12 +135,12 @@ export default async function handler(req: NextRequest) {
       Answer as markdown (including related code snippets if available):
     `
 
-    const completionOptions: ChatCompletionResponseMessage = {
+    const completionOptions: CreateChatCompletionRequest = {
       model: 'gpt-4',
       messages: [{ role: "assistant", content: prompt }],
-      //max_tokens: 1000,
-      // temperature: 0.1,
-      // stream: true,
+      max_tokens: 1000,
+      temperature: 0.1,
+      stream: true,
     }
 
     const response = await fetch('https://api.openai.com/v1/chat/completions', {
