cb6c7007b04dbbf45578c7ef7a504ed359cb181b
dyoon <11621570+davidyoondrums@users.noreply.github.com>
2023-04-29 01:34:48 -0400

    fix: change from message to text

--- components/SearchDialog.tsx
+++ components/SearchDialog.tsx
@@ -134,7 +134,7 @@ export function SearchDialog() {
           }
 
           const completionResponse: CreateCompletionResponse = JSON.parse(e.data)
-          const text = completionResponse.choices[0].message
+          const text = completionResponse.choices[0].text
 
           setAnswer((answer) => {
             const currentAnswer = answer ?? ''
